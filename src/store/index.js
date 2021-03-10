1107fd82077e22dd195d5918254082b0466352e2
Nus <you@example.com>
2021-03-10 22:16:51 +0700

    commit

--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,7 @@ export default new Vuex.Store({
   state: {
   i1:{
       name:"MIDORI ทวินไซส์ สีเทาเข้ม",
-      price:"2550",
+      price:"1000",
       amount:0,
       image:"https://backend.central.co.th/media/catalog/product/C/D/CDS13624156.jpg?impolicy=resize&width=553"
   },
@@ -19,25 +19,25 @@ export default new Vuex.Store({
   },
   i3:{
       name:"MIDORI คิงไซส์ SAN",
-      price:"1290",
+      price:"1250",
       amount:0,
       image:"https://backend.central.co.th/media/catalog/product/C/D/CDS12683949.jpg?impolicy=resize&width=553"
   },
   i4:{
       name:"MIDORI ควีนไซส์ สีน้ำตาลเข้ม",
-      price:"2990",
+      price:"1500",
       amount:0,
       image:"https://backend.central.co.th/media/catalog/product/C/D/CDS13623746.jpg?impolicy=resize&width=553"
   },
   i5:{
       name:"MIDORI คิงไซส์ สีทอง",
-      price:"1790",
+      price:"1700",
       amount:0,
       image:"https://backend.central.co.th/media/catalog/product/C/D/CDS13623692.jpg?impolicy=resize&width=553"
   },
   i6:{
       name:"MIDORI คิงไซส์ London",
-      price:"1790",
+      price:"1700",
       amount:0,
       image:"https://backend.central.co.th/media/catalog/product/C/D/CDS13963972.jpg?impolicy=resize&width=553"
   },
@@ -49,31 +49,31 @@ export default new Vuex.Store({
   },
   i8:{
       name:"MIDORI ทวินไซส์ USA",
-      price:"1490",
+      price:"1400",
       amount:0,
       image:"https://backend.central.co.th/media/catalog/product/C/D/CDS13157616.jpg?impolicy=resize&width=553"
   },
   i9:{
       name:"MIDORI ทวินไซส์ ลายดอกA",
-      price:"790",
+      price:"700",
       amount:0,
       image:"https://backend.central.co.th/media/catalog/product/C/D/CDS12684090.jpg?impolicy=resize&width=553"
   },
   i10:{
     name:"MIDORI ทวินไซส์ ลายดอกB",
-    price:"790",
+    price:"700",
     amount:0,
     image:"https://backend.central.co.th/media/catalog/product/C/D/CDS13157692.jpg?impolicy=resize&width=553"
   },
   i11:{
     name:"MIDORI ทวินไซส์ ลายพิมพ์A",
-    price:"1990",
+    price:"1900",
     amount:0,
     image:"https://backend.central.co.th/media/catalog/product/C/D/CDS12684168.jpg?impolicy=resize&width=553"
   },
   i12:{
     name:"MIDORI ทวินไซส์ ลายพิมพ์B",
-    price:"1990",
+    price:"1900",
     amount:0,
     image:"https://backend.central.co.th/media/catalog/product/C/D/CDS13157760.jpg?impolicy=resize&width=553"
   },
